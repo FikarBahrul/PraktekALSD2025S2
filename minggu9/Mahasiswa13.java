02ff11ffe2d961e92b3bec5918a009eb5b28e200
FikarBahrul <fikarbahrul99@gmail.com>
2025-04-29 09:18:59 +0700

    Penambahan Minggu 9

---
+++ minggu9/Mahasiswa13.java
@@ -0,0 +1,18 @@
+package minggu9;
+
+public class Mahasiswa13 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa13(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+    void tugasDinilai(int nilai){
+        this.nilai = nilai;
+    }
+}
